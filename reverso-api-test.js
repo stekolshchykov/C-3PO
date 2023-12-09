198f8760684d0709864dc5b5733b2181a03c7b3b
mk <v.a.stekolschikov@gmail.com>
2023-12-09 14:28:24 +0000

    init v2

---
+++ reverso-api-test.js
@@ -0,0 +1,33 @@
+const Reverso = require('reverso-api')
+const reverso = new Reverso()
+
+// reverso.getContext(
+//     'meet me half way',
+//     'english',
+//     'russian',
+//     (err, response) => {
+//         if (err) throw new Error(err.message)
+//
+//         console.log(response)
+//     }
+// )
+
+// reverso.getSpellCheck('helo man', 'english', (err, response) => {
+//     if (err) throw new Error(err.message)
+//
+//     // console.log(response)
+//     console.log(JSON.stringify(response.corrections))
+// })
+
+
+// reverso.getSynonyms('Привет', 'russian', (err, response) => {
+//     if (err) throw new Error(err.message)
+//
+//     console.log(response)
+// })
+
+// reverso.getConjugation('идти', 'russian', (err, response) => {
+//     if (err) throw new Error(err.message)
+//
+//     console.log(response)
+// })
