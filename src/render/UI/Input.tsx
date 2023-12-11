32b5afb2556bf89a329e1033d98f5e66a9fbef5f
mk <v.a.stekolschikov@gmail.com>
2023-12-11 21:22:00 +0000

    tmp

--- src/render/UI/Input.tsx
+++ src/render/UI/Input.tsx
@@ -1,14 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 
 interface Props {
     width: number
     placeholder: string
+    onChange: (text: string) => void
 }
 
-const Input = ({width, placeholder}: Props) => {
+const Input = ({width, placeholder, onChange}: Props) => {
+
+    const [text, setText] = useState("")
 
     return <>
         <input
+            value={text}
+            onChange={(event) => {
+                setText(event.target.value)
+                onChange(event.target.value)
+            }}
             className={`w-[${width}%] px-4 py-2 outline-0 rounded flex border-0 m-0 bg-gray focus:bg-grayDark transition`}
             placeholder={placeholder}
         />
--- src/render/features/сontext/Сontext.tsx
+++ src/render/features/сontext/Сontext.tsx
@@ -1,10 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
 import Input from "../../UI/Input";
 import Btn from "../../UI/Btn";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRight, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+// @ts-ignore
+import Reverso from "reverso-api";
+
+const reverso = new Reverso()
 
 const Context = () => {
+
+    const [text, setText] = useState("")
+    const [selectMode, setSelectMode] = useState<null | "from" | "to">(null)
+    const [langFrom, setLangFrom] = useState("russian")
+    const [langTo, setLangTo] = useState("english")
+
+    const lanList = [
+        'arabic',
+        'german',
+        'spanish',
+        'french',
+        'hebrew',
+        'italian',
+        'japanese',
+        'dutch',
+        'polish',
+        'portuguese',
+        'romanian',
+        'russian',
+        'turkish',
+        'chinese',
+        'english'
+    ]
 
     const data: {
         l1: string
@@ -60,35 +87,79 @@ const Context = () => {
         }
     ]
 
+    const selectModeHandler = (l: null | "from" | "to") => {
+        setSelectMode(selectMode !== l ? l : null)
+    }
+
+    const selectLang = (l: string, position: "from" | "to" | null) => {
+        selectModeHandler(null)
+        if (position) {
+            if (position === "from") {
+                setLangFrom(l)
+            } else {
+                setLangTo(l)
+            }
+        }
+    }
+
+    const getContextHandler = () => {
+        reverso.getContext(
+            text,
+            langFrom,
+            langTo,
+            (err: any, response: {
+                examples: { id: number, source: string, target: string }[]
+            }) => {
+                const examples = response?.examples
+                if (examples) {
+                    examples.forEach(e => {
+                        console.log(e)
+                    })
+                }
+            }
+        )
+    }
+
     return <div className={"px-2 pt-4"}>
 
         <div className={"flex items-center gap-2"}>
-            <Input placeholder={"Enter word"} width={100}/>
+            <Input placeholder={"Enter word"} width={100} onChange={e => setText(e)}/>
             <div className={"w-[15px]"}></div>
-            <Btn type={"normal"} size={1} clickHandler={() => {
-                // TODO: need action
-            }}>English</Btn>
+            <Btn type={"normal"} size={1} clickHandler={() => selectModeHandler("from")}>
+                <div className={"capitalize"}>{langFrom}</div>
+            </Btn>
             <div className={"text-center"}>
                 <FontAwesomeIcon icon={faArrowRight}/>
             </div>
-            <Btn type={"normal"} size={1} clickHandler={() => {
-                // TODO: need action
-            }}>Russian</Btn>
-            <Btn type={"normal"} size={1} clickHandler={() => {
-                // TODO: need action
-            }}>
+            <Btn type={"normal"} size={1} clickHandler={() => selectModeHandler("to")}>
+                <div className={"capitalize"}>{langTo}</div>
+            </Btn>
+            <Btn type={"normal"} size={1} clickHandler={() => getContextHandler()}>
                 <FontAwesomeIcon icon={faMagnifyingGlass}/>
             </Btn>
         </div>
-        <ul className={"my-3 h-[395px] overflow-auto"}>
-            {data.map((e, i) => {
-                return <li key={i} className={"grid grid-cols-2 mb-2"}>
-                    <div className={"border-l-4 border-grayDark pl-3"}>{e.l1}</div>
-                    <div className={"border-l-4 border-grayDark pl-3"}>{e.l2}</div>
-                </li>
-            })}
+        <div className={"my-3 h-[395px] overflow-auto relative"}>
+            {selectMode === null && <ul>
+                {data.map((e, i) => {
+                    return <li key={i} className={"grid grid-cols-2 mb-2"}>
+                        <div className={"border-l-4 border-grayDark pl-3"}
+                             onClick={() => selectModeHandler("from")}>{e.l1}</div>
+                        <div className={"border-l-4 border-grayDark pl-3"}
+                             onClick={() => selectModeHandler("to")}>{e.l2}</div>
+                    </li>
+                })}
+            </ul>}
+            {selectMode !== null &&
+                <ul className={"pt-3 absolute top-0 left-0 bg-grayDark w-full h-full m-0 p-0 grid grid-cols-[1fr_1fr_1fr_1fr_1fr] auto-rows-min gap-3 "}>
+                    {lanList.map((e, i) => {
+                        return <li key={i} className={"text-center flex h-[35.5px] justify-center"}>
+                            <Btn type={"normal"} size={1} clickHandler={() => selectLang(e, selectMode)}>{e}</Btn>
+                        </li>
+                    })}
 
-        </ul>
+                </ul>}
+        </div>
+
     </div>
 }
 
