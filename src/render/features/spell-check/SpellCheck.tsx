6c09ad2f24916a7522cdceefc7c09fc4d6e80b08
mk <v.a.stekolschikov@gmail.com>
2023-12-12 16:54:38 +0000

    added SpellCheck functional

--- src/render/features/spell-check/SpellCheck.tsx
+++ src/render/features/spell-check/SpellCheck.tsx
@@ -3,7 +3,7 @@ import Textarea from "../../UI/Textarea";
 import Btn from "../../UI/Btn";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCaretDown} from "@fortawesome/free-solid-svg-icons";
-import {reverso} from "../../reverso";
+import {availableLang, reverso} from "../../reverso";
 
 interface ICorrection {
     explanation?: string,
@@ -21,6 +21,7 @@ const SpellCheck = () => {
     const [text, setText] = useState<string>("")
     const [corrections, setCorrections] = useState<ICorrection[]>([])
     const [lang, setLang] = useState("english")
+    const [selectMode, setSelectMode] = useState<boolean>(false)
 
     const spellCheckHandler = async () => {
         try {
@@ -51,22 +52,42 @@ const SpellCheck = () => {
             <Textarea rows={4} placeholder={"Enter expression"} onChange={(e) => setText(e)}/>
 
             <div className={"flex gap-2 justify-end pt-3"}>
-                <Btn type={"normal"}>Russian <FontAwesomeIcon icon={faCaretDown}/></Btn>
+                <Btn type={"normal"} clickHandler={() => setSelectMode(!selectMode)}>
+                    <div className={"capitalize"}>{lang}</div>
+                    <FontAwesomeIcon icon={faCaretDown}/>
+                </Btn>
                 <Btn type={"normal"} clickHandler={spellCheckHandler}>Check</Btn>
             </div>
 
-            <div className={"text-lg pt-3"}>Corrections({corrections.length}):</div>
+            <div className={"relative h-[275px] overflow-auto mt-[15px]"}>
+                {selectMode === false && <>
+                    <div className={"text-lg pt-3"}>Corrections({corrections.length}):</div>
+                    <ul className={"pt-3 pb-2 grid grid-cols-1 gap-2"}>
+                        {corrections.map((e, i) =>
+                            <li key={i} className={"px-3 py-3 border-l-4 border-grayDark"}>
+                                {e.explanation}: {e.corrected}
+                                {e?.suggestions.length && <div>Suggestions: {e?.suggestions?.map(((ee, i) =>
+                                        <span key={i + ee.text}>{ee.text}{e?.suggestions?.length > i + 1 && ", "}</span>
+                                ))}</div>}
+                            </li>
+                        )}
+                    </ul>
+                </>}
+                {selectMode &&
+                    <ul className={"pt-3 absolute top-0 left-0 bg-grayDark w-full h-full m-0 p-0 grid grid-cols-[1fr_1fr_1fr_1fr_1fr] auto-rows-min gap-3 "}>
+                        {availableLang.spell.map((e, i) => {
+                            return <li key={i} className={"text-center flex h-[35.5px] justify-center"}>
+                                <Btn type={"normal"} size={1} clickHandler={() => {
+                                    setLang(e)
+                                    setSelectMode(false)
+                                }}>
+                                    <div className={"capitalize"}>{e}</div>
+                                </Btn>
+                            </li>
+                        })}
+                    </ul>}
+            </div>
 
-            <ul className={"pt-3 pb-2 grid grid-cols-1 gap-2 h-[275px] overflow-auto"}>
-                {corrections.map((e, i) =>
-                    <li key={i} className={"px-3 py-3 border-l-4 border-grayDark cursor-pointer"}>
-                        {e.explanation}: {e.corrected}
-                        <div>Suggestions: {e?.suggestions?.map(((ee, i) =>
-                                <span key={i + ee.text}>{ee.text}{e?.suggestions?.length > i + 1 && ", "} </span>
-                        ))}</div>
-                    </li>
-                )}
-            </ul>
 
         </div>
     </div>
