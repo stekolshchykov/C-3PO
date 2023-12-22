19ba2e137cd009ac2458e2162366b601ea4f13d3
mk <v.a.stekolschikov@gmail.com>
2023-12-22 11:41:25 +0000

    fixed css

--- src/render/components/PageLasyout.tsx
+++ src/render/components/PageLasyout.tsx
@@ -16,7 +16,7 @@ const PageLayout: React.FC<Props> = ({children, title}) => {
         className={"bg-grey pb-5 grid grid-rows-[min-content_1fr] grid-cols-[1fr] h-[100vh] overflow-hidden"}>
         <div
             className={"grid grid-cols-[1fr_1fr_1fr] justify-evenly align-middle my-0 mx-0 px-3 h-[60px] bg-gray items-center"}>
-            <div onClick={() => navigate(-1)}>
+            <div onClick={() => navigate(-1)} className={"cursor-pointer"}>
                 <FontAwesomeIcon icon={faAngleLeft} size={"2x"}/>
             </div>
             <div className={"text-center text-2xl m-auto"}>{title}</div>
