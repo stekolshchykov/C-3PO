384b931bfcc5592ced794b0e70020e7f7ecef484
mk <v.a.stekolschikov@gmail.com>
2023-12-22 16:52:46 +0000

    fix

--- src/main/function.ts
+++ src/main/function.ts
@@ -3,16 +3,18 @@ import {IConfig} from "../type";
 import {SystemStore} from "./SystemStore";
 
 const systemStore = new SystemStore()
-
+let windowWidth = 600
 export const showWindow = async (tray: Tray, mainWindow: BrowserWindow) => {
-    const c: IConfig = await systemStore.get("config")
-    let windowWidth = 600
-    if (c && c?.windowWidth) windowWidth = c?.windowWidth
+
     const {x, y} = tray.getBounds();
     const trayWidth = tray.getBounds().width;
     const trayHeight = tray.getBounds().height;
     const xPos = x + trayWidth / 2 - windowWidth / 2;
     const yPos = y - trayHeight;
     mainWindow.setPosition(xPos, yPos);
-    mainWindow.show();
-};
+    mainWindow.show()
+
+    const c: IConfig = await systemStore.get("config")
+    if (c && c?.windowWidth) windowWidth = c?.windowWidth
+
+}
