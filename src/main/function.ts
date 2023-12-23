1bdf491d755dcccdf02b940cc3675f4da26b8085
mk <v.a.stekolschikov@gmail.com>
2023-12-23 10:54:12 +0000

    fixed the window opening bug

--- src/main/function.ts
+++ src/main/function.ts
@@ -1,20 +1,14 @@
 import {BrowserWindow, Tray} from "electron";
-import {IConfig} from "../type";
-import {SystemStore} from "./SystemStore";
 
-const systemStore = new SystemStore()
-let windowWidth = 600
+
 export const showWindow = async (tray: Tray, mainWindow: BrowserWindow) => {
 
     const {x, y} = tray.getBounds();
     const trayWidth = tray.getBounds().width;
     const trayHeight = tray.getBounds().height;
-    const xPos = x + trayWidth / 2 - windowWidth / 2;
+    const xPos = x + trayWidth / 2 - mainWindow.getContentSize()[0] / 2;
     const yPos = y - trayHeight;
     mainWindow.setPosition(xPos, yPos);
     mainWindow.show()
-
-    const c: IConfig = await systemStore.get("config")
-    if (c && c?.windowWidth) windowWidth = c?.windowWidth
 
 }
