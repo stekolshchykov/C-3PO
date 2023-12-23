e227c49c1ebd8094833654b3af8c448e4dd0078f
mk <v.a.stekolschikov@gmail.com>
2023-12-23 12:12:46 +0000

    added description

---
+++ README.md
@@ -0,0 +1,18 @@
+# C-3PO
+
+## Overview
+
+This lightweight tray application simplifies language-related tasks, offering quick access to essential features via
+hotkeys. Enhance your workflow with text translation, spell-checking, Wikipedia lookups, contextual translation, synonym
+finding, and part-of-speech identification.
+
+## Features
+
+- **Translate Text:** Translate text seamlessly using hotkeys.
+- **Spell Check:** Ensure accuracy with real-time spell checking.
+- **Wikipedia Lookup:** Retrieve information from Wikipedia effortlessly.
+- **Contextual Translation:** Gain insight with contextual translations.
+- **Find Synonyms:** Enhance your language with quick synonym suggestions.
+- **Part-of-Speech Identification:** Identify the part of speech of selected words.
+
+
--- test.ts
+++ test.ts
@@ -1,3 +1,23 @@
+// I'm developing an online translator app, and this is what I found out empirically:
+//
+// sl - source language code (auto for autodetection)
+// tl - translation language
+// q - source text / word
+// ie - input encoding (a guess)
+// oe - output encoding (a guess)
+// dt - may be included more than once and specifies what to return in the reply.
+//     Here are some values for dt. If the value is set, the following data will be returned:
+//
+//     t - translation of source text
+// at - alternate translations
+// rm - transcription / transliteration of source and translated texts
+// bd - dictionary, in case source text is one word (you get translations with articles, reverse translations, etc.)
+// md - definitions of source text, if it's one word
+// ss - synonyms of source text, if it's one word
+// ex - examples
+// rw - See also list.
+//     dj - Json response with names. (dj=1)
+
 // // // import Store from "electron-store";
 // // //
 // // // const store = new Store()
